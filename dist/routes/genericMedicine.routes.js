ba730d02d15b44221134185fa010e258b02ff8b8
JESVIN JOSE <jesvinjose49@gmail.com>
2025-09-05 23:37:10 +0530

    deleted files in dist

--- dist/routes/genericMedicine.routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { upload } from "../utils/multer"; // ← import multer config
-import { uploadCsvAndSaveToDB } from "../controllers/genericMedicine.controller";
-const router = express.Router();
-// Use multer middleware here
-router.post("/upload_generic", upload.single("file"), uploadCsvAndSaveToDB);
-export default router;
--- dist/routes/pharmaBranch.routes.js
+++
@@ -1,7 +0,0 @@
-// routes/pharmaBranch.routes.ts
-import express from "express";
-import { listBranchOrders } from "../controllers/pharmaBranch.controller";
-import { authenticate, authorizeRoles, } from "../middleware/authenticate.middleware";
-const router = express.Router();
-router.post("/list_branch_orders", authenticate, authorizeRoles(["pharma_branch"]), listBranchOrders);
-export default router;
